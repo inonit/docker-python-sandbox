0a42d8786c9d30353fcaa30d630c5c42ec2ea01f
bastien mariyathas <bastien.mariyathas@gmail.com>
2019-11-25 13:23:58 +0100

    Replace child-process by python-shell. Enabled json format sending-receiving data in running python script

--- container/shared/index.js
+++ container/shared/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var fs = require('fs');
-var child_process = require('child_process');
+const {PythonShell} = require("python-shell");
 var _ = require('underscore');
 
 var app = express();
@@ -11,47 +11,59 @@ var port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.post('/', function (req, res) {
-    
+
     res.setHeader('Content-Type', 'application/json');
-    
+
   	if (!req.body.code || !req.body.timeoutMs) {
         res.status(400);
         res.end(JSON.stringify({error: "no code or timeout specified"}));
   	}
   	else {
   	    res.status(200);
-    
+
   		// Write code to file
-  		fs.writeFileSync('./code.py', req.body.code);
-  		
-		var executor = (req.body.v3 === true) ? "python3" : "python"
-  		var job = child_process.spawn(executor, ["-u", "./code.py"], { cwd: __dirname })
-  		var output = {stdout: '', stderr: '', combined: ''};
-  		
-  		job.stdout.on('data', function (data) {
-  		    output.stdout += data;
-  		    output.combined += data;
-  		})
-  		
-  		job.stderr.on('data', function (data) {
-  		    output.stderr += data;
-  		    output.combined += data;
-  		})
-  	
-    	// Timeout logic
+      fs.writeFileSync('./code.py', req.body.code);
+      var output = {stdout: {}, stderr: {}/*, combined: ''*/};
+      var options={
+        mode: 'json',
+        pythonPath: req.body.v3 ? 'python3' : 'python',
+      };
+      var pyshell = new PythonShell('./code.py', options);
+      // sends a message to the Python script via stdin
+      if (req.body.data){
+        pyshell.send(req.body.data); // must be json format or have to use json.parse
+      }
+
+      // Timeout logic
   		var timeoutCheck = setTimeout(function () {
-  		    console.error("Process timed out. Killing")
-  		    job.kill('SIGKILL');
-  		    var result = _.extend(output, { timedOut: true, isError: true, killedByContainer: true });
-  		    res.end(JSON.stringify(result));
-  		}, req.body.timeoutMs)
-  		
-  		job.on('close', function (exitCode) {
-  		   var result = _.extend(output, { isError: exitCode != 0 })
-  		   res.end(JSON.stringify(result));
-  		   clearTimeout(timeoutCheck);
-  		});
-  	
+          console.error("Process timed out. Killing")
+          pyshell.terminate('SIGKILL');
+      }, req.body.timeoutMs)
+
+      pyshell.on('message', function (message) {
+        // should receive only one json response data
+        output.stdout = message;
+        //output.combined += message;
+
+      }).on('stderr', function (stderr) {
+        // handle stderr (a line of text from stderr)
+        output.stderr = stderr;
+        //output.combined += message;
+
+      }).end(function (err,code,signal) {
+        if (err){
+          // err handled in 'stderr' event
+        }
+        if (pyshell.exitSignal == 'SIGKILL' || signal == 'SIGKILL'){
+          var result = _.extend(output, { timedOut: true, isError: true, killedByContainer: true });
+          res.end(JSON.stringify(result));
+        }
+        else{
+          var result = _.extend(output, { timedOut: false, isError: code != 0 })
+          res.end(JSON.stringify(result));
+          clearTimeout(timeoutCheck);
+        }
+      });
   	}
 });
 
